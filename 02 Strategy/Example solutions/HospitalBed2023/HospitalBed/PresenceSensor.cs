c163be612b3dd30b469d6fd5e5e5fad77e3f1c8b
Henrik Kirk <henrikkirk@gmail.com>
2023-08-22 15:05:13 +0200

    HospitalBed for 2023 added

---
+++ 02 Strategy/Example solutions/HospitalBed2023/HospitalBed/PresenceSensor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace HospitalBed
+{
+    internal class PresenceSensor
+    {
+        private readonly IBedControl _bedControl;
+        private readonly Random _random = new Random();
+        public bool Running { get; set; } = true;
+
+        private const int SampleTime = 100;
+
+        public PresenceSensor(IBedControl bedControl)
+        {
+            _bedControl = bedControl;
+        }
+
+        private bool GetSample()
+        {
+            return Convert.ToBoolean(_random.Next(0, 2));  // 0 = false, 1 = true
+        }
+
+        public void Run()
+        {
+            while (Running)
+            {
+                var presenceDetected = GetSample();
+                _bedControl.HandleData(new PresenceSensorDataContainer()
+                {
+                    PresenceDetected = presenceDetected
+                });
+                Thread.Sleep(SampleTime);
+            }
+        }
+    }
+}
