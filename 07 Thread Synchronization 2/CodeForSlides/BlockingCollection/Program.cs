e62fe074b60a786bd52e47b1aa6e555e3bb123a9
Michael Loft <ml@ece.au.dk>
2025-11-12 10:59:21 +0100

    moved from duplicate folder in git

---
+++ 07 Thread Synchronization 2/CodeForSlides/BlockingCollection/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace BlockingCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlockingCollection<DataContainer> dataQueue = new BlockingCollection<DataContainer>();
+
+            Producer producer = new Producer(dataQueue);
+            Consumer consumer = new Consumer(dataQueue);
+
+            Thread producerThread = new Thread(producer.Run);
+            Thread consumerThread = new Thread(consumer.Run);
+
+            producerThread.Start();
+            consumerThread.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 07 Thread Synchronization 2/CodeForSlides/WindowsForms/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form1 mainForm = new Form1();
+
+            CounterThread counter = new CounterThread(mainForm);
+            Thread counterThread = new Thread(counter.Run);
+            counterThread.IsBackground = true;
+
+            counterThread.Start();
+
+            Application.Run(mainForm);
+        }
+    }
+}
